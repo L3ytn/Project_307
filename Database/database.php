189ee0e9474c60901e3acfffc0b567397f0adedf
L3ytn <93453017+L3ytn@users.noreply.github.com>
2021-11-03 09:04:54 +0100

    Update database.php

--- Database/database.php
+++ Database/database.php
@@ -1,5 +1,5 @@
 <?php
-	$configuration_file_contents = file_get_contents("config/configuration.json")
+	$configuration_file_contents = file_get_contents("config/configuration.json");
 	$configuration = json_decode($configuration_file_contents);
 	echo $configuration->hostname;
 
