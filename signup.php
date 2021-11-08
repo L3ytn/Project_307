1d955166f86b1e82a8cea65f98168aba55a251e8
L3ytn <Leytomio17@gmail.com>
2021-11-08 11:58:22 +0100

    First version of the structure.

---
+++ signup.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<h1>Sign-up</h1>
+	<p>Filling in the form will create an account!</p>
+
+	<label for="username">Username</b></label>
+	<input type="text" placeholder="Username" id="username-text" required="True">
+
+	<label for="psw">Password</b></label>
+	<input type="password" placeholder="Password" id="password-text" required="True">
+
+	<label for="repeat-password">Re-Type Password</b></label>
+	<input type="password" placeholder="Repeat Password" id="repeat-password-text" required="True">
+
+
+	<button id="cancel-button">Cancel</button>
+	<button id="sign-up-Button">Sign-up</button>
+
+</body>
+</html>
